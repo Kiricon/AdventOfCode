b207de141ab9b12b56ee4805c465e4828d91cc53
kiricon <kiricon@live.com>
2016-10-09 21:37:13 -0600

    Setup project and I'm getting a little close

---
+++ Day 13: Knights of the Dinner Table/main.py
@@ -0,0 +1,105 @@
+with open("Input.txt") as f:
+    content = f.readlines()
+
+
+nodes = []
+people = []
+global answer
+global pathTaken
+global answerHistory
+pathTaken = []
+answer = 0
+answerHistory = []
+##### Create list of people
+for line in content:
+    line = line.replace('\n', '')
+    line = line.replace('.', '')
+    arr = line.split(' ')
+
+    if arr[0] not in people:
+        people.append(arr[0])
+
+#### Create Nodes
+for person in people:
+    node = {'name': person, 'locs': []}
+    for line in content:
+        line = line.replace('\n', '')
+        line = line.replace('.', '')
+        arr = line.split(' ')
+        if arr[0] == person:
+            loc = {}
+            if arr[2] == "gain":
+                loc = {'name': arr[10], 'units': int(arr[3])}
+            else:
+                loc = {'name': arr[10], 'units': -int(arr[3])}
+            node.get('locs').append(loc)
+    nodes.append(node)
+
+#### Find Node by name
+def findNode(name):
+    for n in nodes:
+        if n.get('name') == name:
+            nextNode = n
+    return nextNode
+
+#### Find best person to sit next to!
+def findBest(node, seated, total, totalHistory):
+    for loc in node.get('locs'):
+        global answer
+        global pathTaken
+        global answerHistory
+        newTotal = total
+        newSeated = seated[:]
+
+        newHistory = totalHistory[:]
+        print loc
+        if loc.get('name') not in newSeated:
+            ### Find other person's happy
+            nextNode = findNode(loc.get('name'))
+            other = nextNode
+            otherHappy = 0
+            for otherLoc in other.get('locs'):
+                if node.get('name') == otherLoc.get('name'):
+                    print otherLoc
+                    otherHappy = otherLoc.get('units')
+
+            happy = loc.get('units') + otherLoc.get('units');
+
+            newHistory.append(happy)
+            newTotal += happy
+            newSeated.append(nextNode.get('name'))
+
+            if len(newSeated) == len(people):
+                if newTotal > answer:
+                    answer = newTotal
+                    pathTaken = seated
+                    answerHistory = newHistory
+            else:
+                findBest(nextNode, newSeated, newTotal, newHistory)
+
+
+
+def loop(node, seated, total, totalHistory):
+    for loc in node.get('locs'):
+        global answer
+        global pathTaken
+        global answerHistory
+        newTotal = total
+        newSeated = seated[:]
+        nextNode = findNode(loc.get('name'))
+        newHistory = totalHistory[:]
+
+
+
+## Loop through each node and try a few combos
+for node in nodes:
+
+    seated = [node.get('name')]
+    total = 0
+    totalHistory = []
+
+    findBest(node, seated[:], total, totalHistory[:])
+
+print answer
+print answerHistory
+print pathTaken
