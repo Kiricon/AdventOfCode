7c691c181d886c5af9926d7999e4af2f7fe03fb3
kiricon <kiricon@live.com>
2016-10-11 21:19:45 -0600

    Built setup for algorithm I think will find the answer

--- Day 15: Science for Hungry People/main.py
+++ Day 15: Science for Hungry People/main.py
@@ -22,4 +22,17 @@ for line in content:
     line = line.replace(',', '')
 
     arr = line.split(' ')
-    Ingredient.append(Ingredient(int(arr[2]), int(arr[4]), int(arr[6]), int(arr[8]), int(arr[10]), arr[0] ))
+    Ingredients.append(Ingredient(int(arr[2]), int(arr[4]), int(arr[6]), int(arr[8]), int(arr[10]), arr[0] ))
+
+####### Setup is complete, time for real code
+
+room = 100
+used = []
+for ing in Ingredients:
+    used.append({'name': ing.name, 'total': room/len(Ingredients)})
+
+switch = 0;
+lastTotal = 0;
+while switch < len(Ingredients):
+
+    newUsed = used[:]
